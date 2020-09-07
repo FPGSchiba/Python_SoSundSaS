4805a0ce2ece2784a9a6d8f07e74a7727ff19f12
FPGSchiba <59923683+FPGSchiba@users.noreply.github.com>
2020-09-07 17:25:52 +0200

    Scan und Dashboards Fertig

--- MonitorIT/Functions/ScanHardware.py
+++ MonitorIT/Functions/ScanHardware.py
@@ -2,5 +2,68 @@
 # Author: Jann Erhardt
 # Build: 1.0
 
+import psutil
+import GPUtil
+from tabulate import tabulate
+
+
+def get_size(bytes):
+    factor = 1024
+    for unit in ["", "K", "M", "G", "T", "P"]:
+        if bytes < factor:
+            return f"{bytes:.2f}"
+        bytes /= factor
+
+
 def CPU_Precent():
-    pres = 0
+    return psutil.cpu_percent()
+
+
+def MEM_Precent():
+    swap = psutil.swap_memory()
+    return swap.percent
+
+
+def DISK_Usage():
+    partitions = psutil.disk_partitions()
+    for partition in partitions:
+        try:
+            partition_usage = psutil.disk_usage(partition.mountpoint)
+        except PermissionError:
+            # this can be catched due to the disk that
+            # isn't ready
+            continue
+        return partition_usage.percent
+
+
+def DISK_Free():
+    partitions = psutil.disk_partitions()
+    for partition in partitions:
+        try:
+            partition_usage = psutil.disk_usage(partition.mountpoint)
+        except PermissionError:
+            continue
+        return int(float(get_size(partition_usage.free)))
+
+
+def DISK_Max():
+    partitions = psutil.disk_partitions()
+    for partition in partitions:
+        try:
+            partition_usage = psutil.disk_usage(partition.mountpoint)
+        except PermissionError:
+            continue
+        return int(float(get_size(partition_usage.total)))
+
+
+def GPU_Usage():
+    gpus = GPUtil.getGPUs()
+    list_gpus = []
+    for gpu in gpus:
+        print("SoS")
+        gpu_load = f"{gpu.load * 100}%"
+        gpu_temperature = f"{gpu.temperature} °C"
+        list_gpus.append((
+            f"GPU-Load: {gpu_load}", f"GPU-Temp: {gpu_temperature}"
+        ))
+    return list_gpus
---
+++ MonitorIT/Test/TestFunctions.py
@@ -0,0 +1,21 @@
+# Version 1.0
+# Author: Jann Erhardt
+# Build: 1.0
+
+from Functions.ScanHardware import *
+import time
+from datetime import datetime
+
+# while True:
+#     time.sleep(1)
+#     print("=" * 30)
+#     print(CPU_Precent())
+#     print(MEM_Precent())
+#     print(DISK_Usage())
+#     print(DISK_Free())
+#     try:
+#         print(GPU_Usage()[0])
+#         print(GPU_Usage()[1])
+#     except:
+#         continue
+
