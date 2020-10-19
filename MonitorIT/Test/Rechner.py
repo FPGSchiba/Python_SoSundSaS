d3b83e2b11e99d7070abfc9bf5feaa5634ee92cd
FPGSchiba <59923683+FPGSchiba@users.noreply.github.com>
2020-10-19 08:18:17 +0200

    Update Rechner.py

--- MonitorIT/Test/Rechner.py
+++ MonitorIT/Test/Rechner.py
@@ -2,10 +2,35 @@
 
 while True:
 	print("Willkommen zum Rechner")
+	print("======================")
 	print(" 1 - Plus")
 	print(" 2 - Minus")
 	print(" 3 - Mal")
-	print(" 4 - Durch")
+	print(" 4 - Durch \n")
+	print(" 0 - Beenden")
 	
 	# Eingabe
-	eing = int(input("Wählen Sie: ")
+	try:
+		eing = int(input("Wählen Sie: "))
+	except:
+		input("input error try again.")
+		eing = 5
+	if(not eing == 5):
+		first_number = int(input("Bitte die erste Zahl eingeben: "))
+		second_number = int(input("Bitte die zweite Zahl eingeben: "))		
+
+		out = 0
+
+		# Verarbeitung
+		if(eing == 1):
+			out = first_number + second_number
+		if(eing == 2):
+			out = first_number - second_number
+		if(eing == 3):
+			out =  first_number * second_number
+		if(eing == 4):
+			out = first_number / second_number
+		if(eing == 0):
+			break
+		
+		input("Das ist das Resultat: " + str(out))
