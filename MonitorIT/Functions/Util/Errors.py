40c23dc1cfd040aa9f7778138bb43f7e86f6b121
FPGSchiba <59923683+FPGSchiba@users.noreply.github.com>
2020-11-23 14:40:52 +0100

    moved MIT to other Project

--- MonitorIT/Functions/Util/Errors.py
+++
@@ -1,7 +0,0 @@
-class DisconnectedError(Exception):
-    def __init__(self, message, errors):
-        # Call the base class constructor with the parameters it needs
-        super(DisconnectedError, self).__init__(message)
-        self.errors = errors
-        self.message = "Socket disconnected unexpected"
-        pass
