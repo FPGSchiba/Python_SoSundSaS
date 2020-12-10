f7bb3382fa120025de2cb2d5bf35974af9d1aa6e
FPGSchiba <59923683+FPGSchiba@users.noreply.github.com>
2020-12-10 16:52:07 +0100

    ML Basics

--- AI/Tests/Function_Test.py
+++ AI/Tests/Function_Test.py
@@ -1,3 +1,17 @@
-from Functions.Random import CreateNewUserAruba
+from __future__ import absolute_import, division, print_function, unicode_literals
 
-CreateNewUserAruba.CreateNewUser()
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from IPython.display import clear_output
+from six.moves import urllib
+
+from tensorflow import feature_column as fc
+import tensorflow as tf
+
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.INFO)
+
+
